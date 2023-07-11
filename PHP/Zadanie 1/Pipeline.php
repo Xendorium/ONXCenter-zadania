1892d8936640a0cd9f3301a5b17cf4468f3d2059
Xendorium <127499049+Xendorium@users.noreply.github.com>
2023-07-11 22:35:30 +0200

    Add files via upload

--- PHP/Zadanie 1/Pipeline.php
+++ PHP/Zadanie 1/Pipeline.php
@@ -1,9 +1,12 @@
 <?php
-
+session_start();
 class Pipeline
 {
-    public static function make(...$functions){
-        //$arg=0;
-        echo "Dobra droga";
+    public static function make($function){
+
+        $closure = function($arg) use ($function){
+            $function($arg);
+        };
+        $closure($_SESSION['var']);
     }
 }
--- PHP/Zadanie 1/index.php
+++ PHP/Zadanie 1/index.php
@@ -1,7 +1,9 @@
 <?php
 include_once ('Pipeline.php');
 
-$var=3;
+$var = 0;
 
-    Pipeline::make(function($var) { return $var * 3; }, function($var) { return $var + 1; },
-    function($var) { return $var / 2; });
+$_SESSION['var'] = $var;
+
+Pipeline::make(function($var) { return $var * 3; }/*, function($var) { return $var + 1; },
+function($var) { return $var / 2; }*/);
