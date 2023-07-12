367b5ad23a4303a8e1d33ef566e45905260a1e28
Xendorium <127499049+Xendorium@users.noreply.github.com>
2023-07-12 13:09:28 +0200

    Stworzenie tablicy asocjacyjnej (imiona=>punkty)

--- PHP/zadanie 3/RankingTable.php
+++ PHP/zadanie 3/RankingTable.php
@@ -3,14 +3,24 @@
 class RankingTable
 {
     public $competitors = array();
+    public $rank = array();
     public function __construct($players){
         $this->competitors=$players;
     }
 
-    public function recordResult(){
-
+    public function recordResult($name,$points){
+        for($i=0;$i<count($this->competitors);$i++){
+            if($name==$this->competitors[$i]){
+                goto P1;
+            }
+        }
+        echo "</br>Nie znaleziono gracza ".$name." na liście graczy";
+        return;
+        P1:
+        echo "</br>Znaleziono gracza: ".$name;
+        $this->rank[$name]=$points;
     }
-    public function playerRank(){
+    public function playerRank($rank){
 
     }
 }
--- PHP/zadanie 3/index.php
+++ PHP/zadanie 3/index.php
@@ -2,5 +2,9 @@
 require_once ('RankingTable.php');
 
 $table = new RankingTable(array('Jan', 'Maks', 'Monika'));
+$table->recordResult('Jan', 2);
+$table->recordResult('Maks', 3);
+$table->recordResult('Monika', 5);
 
-echo $table->competitors[2];
+echo "</br>".$table->rank['Jan'];
+
